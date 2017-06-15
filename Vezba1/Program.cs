34cc3464e193f3f0a9f3e1df9a1015cd81fa3822
vladimir <skipic@gmail.com>
2017-06-15 15:38:28 +0200

    Add project files.

---
+++ Vezba1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vezba1
+{
+
+    enum MyEnum
+    {
+        Sasa = 0,
+        Vlado = 1
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine(Enum.GetName(typeof(MyEnum), 1));
+            foreach (var VARIABLE in Enum.GetNames(typeof(MyEnum)))
+            {
+                Console.WriteLine(VARIABLE);
+            }
+
+              
+
+            MyEnum ee;
+            Enum.TryParse("1", out ee);
+
+            Console.WriteLine(ee);
+        }
+
+       
+
+    }
+}
+
+
+ 
+
+ 
