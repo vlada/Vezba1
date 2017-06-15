5b6f5b7d23f33a7e30b8212d9574b0b3c4335695
vladimir <skipic@gmail.com>
2017-06-15 15:54:14 +0200

    1

--- Vezba1/Program.cs
+++ Vezba1/Program.cs
@@ -8,29 +8,15 @@ using System.Threading.Tasks;
 namespace Vezba1
 {
 
-    enum MyEnum
-    {
-        Sasa = 0,
-        Vlado = 1
-    }
-
+    
     class Program
     {
         static void Main(string[] args)
         {
 
-            //Console.WriteLine(Enum.GetName(typeof(MyEnum), 1));
-            foreach (var VARIABLE in Enum.GetNames(typeof(MyEnum)))
-            {
-                Console.WriteLine(VARIABLE);
-            }
+            int[] myArray;
 
-                
 
-            MyEnum ee;
-            Enum.TryParse("1", out ee);
-
-            Console.WriteLine(ee);
         }
 
        
